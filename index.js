dcd7cef1235664f3a3019da1ca53d75c7eb66233
Anastasiya Karpenka <knesty@yandex.by>
2023-04-13 20:01:52 +0300

    objects and arrays

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 var str = 'str';
 console.log(str);
+
+//ассив чисел, в котором каждое число встречается 2 раза и лишь 1 число - 1 раз. Найти это число. 
+let someArr = [2, 3, 1, 4, 3, 4, 1];
+
+someArr.sort();
+console.log(someArr);
+
+let numb = someArr.filter((i, index, arr) => {
+    if (index !== 0 && arr[index] !== arr[index - 1] && arr[index] !== arr[index + 1]) {
+        return i;
+    }
+});
+
+console.log(numb);
+
+
+const time = {
+    hour: 2,
+    seconds: () => {
+        console.log(this);
+        return 3600 * this.hour
+    },
+    minutes() {
+        return 60 * this.hour;
+    }
+}
+
+console.log(time.seconds());
+console.log(time.minutes());
--- index.ts
+++ index.ts
@@ -1,18 +1,57 @@
-const currRate: string = "1.05";
-
-const fetchCurr = (response: string): number => {
-	const data: number = JSON.parse(response);
-	return data;
+const electricityUserData = {
+	readings: 95,
+	units: "kWt",
+	mode: "double",
 };
 
-function transferEurToUsd(available: boolean, amount: number, commission: number) {
-	if (available) {
-		let res: number = fetchCurr(currRate) * amount * commission;
-		console.log(res);
-		// Или запись в элемент на странице вместо консоли
-	} else {
-		console.log("Сейчас обмен недоступен");
-	}
-}
+const waterUserData = {
+	readings: 3,
+	units: "m3",
+};
 
-transferEurToUsd(true, 500, 1.05);
+const elRate: number = 0.45;
+const wRate: number = 2;
+
+const monthPayments: number[] = [0,0];
+
+const calculatePayments = (elData: { 
+							readings: number;
+							mode: string;
+						}, 
+						wData: { 
+							readings: number;
+						}, 
+						elRate: number, 
+						wRate: number): void => {
+	if (elData.mode === "double" && elData.readings < 50) {
+		monthPayments[0] = elData.readings * elRate * 0.7;
+	} else {
+		monthPayments[0] = elData.readings * elRate;
+	}
+
+	monthPayments[1] = wData.readings * wRate;
+};
+
+calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+
+const sendInvoice = ([elData, wData]: number[], 
+	electricityUserData: {
+		readings: number;
+		units: string;
+	}, waterUserData: {
+		readings: number;
+		units: string;
+	}): string => {
+		
+	const text = `    Hello!
+    This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
+    It will cost: ${elData}€
+    
+    This month you used ${waterUserData.readings} ${waterUserData.units} of water
+    It will cost: ${wData}€`;
+
+	return text;
+};
+
+console.log(sendInvoice(monthPayments, electricityUserData, waterUserData));
+
