be2b172c36ed26ea4e0cf465ead4c342235c8855
Anastasiya Karpenka <knesty@yandex.by>
2023-05-24 14:03:58 +0300

    keyof using

--- index.ts
+++ index.ts
@@ -1,126 +1,70 @@
-// Создать Generic-интерфейс PlayerData, который подходил бы для создания таких объектов:
-interface PlayerData<T> {
-	game: string | number;
-	hours: T;
-	server: string;
+interface IPhone {
+	company: string;
+	number: number;
 }
 
-const player1: PlayerData<number> = {
-	game: "CS:GO",
-	hours: 300,
-	server: "basic",
-};
+// IMobilePhone должен наследоваться от IPhone,
+// тип свойства companyPartner зависит от свойства company
 
-const player2: PlayerData<string> = {
-	game: 2048,
-	hours: "300 h.",
-	server: "arcade",
-};
-
-const player3: PlayerData<{total: number, inMenu: number}> = {
-	game: "Chess",
-	hours: {
-		total: 500,
-		inMenu: 50,
-	},
-	server: "chess",
-};
-
-// Массив данных с фигурами содержит объекты, у каждого из которых обязательно есть свойство name
-// Каждый объект может еще содержать дополнительные свойства в случайном виде
-// Свойство name может иметь только 4 варианта
-// Функция calculateAmountOfFigures должна принимать массив с объектами, у которых обязательно должно быть свойство name
-// Возвращает она объект-экземпляр AmountOfFigures
-// Внутри себя подсчитывает сколько каких фигур было в массиве и записывает результаты в AmountOfFigures
-// С текущими данными в консоль должно попадать:
-// { squares: 3, circles: 2, triangles: 2, others: 1 }
-
-enum FigureNames {
-	Rect = "rect",
-	Triangle = "triangle",
-	Line = "line",
-	Circle = "circle"
+interface IMobilePhone extends IPhone {
+	size: string;
+	companyPartner: IPhone['company'];
+	manufactured: Date;
 }
 
-interface IFigureRequire {
-	name: FigureNames;
-}
+// Типизировать объект phones
 
-// function toFigureObj<T extends IFigureRequire>(data: T): T {
-// 	return data;
-// }
-
-interface IFigureAllData <T extends IFigureRequire> {
-	data: T
-}
-
-interface AmountOfFigures {
-	squares: number;
-	circles: number;
-	triangles: number;
-	others: number;
-}
-
-function calculateAmountOfFigures<IFigureAllData>(figure: IFigureAllData[]): AmountOfFigures {
-	let amountOfFigures: AmountOfFigures = {
-		squares: 0,
-		circles: 0,
-		triangles: 0,
-		others: 0
-	}
-
-	figure.forEach((obj: any) => {
-		switch (obj.name) {
-			case FigureNames.Rect:
-				amountOfFigures.squares += 1;
-				break;
-			case FigureNames.Circle:
-				amountOfFigures.circles += 1;
-				break;
-			case FigureNames.Triangle:
-				amountOfFigures.triangles += 1;
-				break;
-			default:
-				amountOfFigures.others += 1;
-				break;
-		}
-	});
-	
-	return amountOfFigures;
-}
-
-const data = [
+const phones: IMobilePhone[] = [
 	{
-		name: "rect",
-		data: { a: 5, b: 10 },
+		company: "Nokia",
+		number: 1285637,
+		size: "5.5",
+		companyPartner: "MobileNokia",
+		manufactured: new Date("2022-09-01"),
 	},
 	{
-		name: "rect",
-		data: { a: 6, b: 11 },
+		company: "Samsung",
+		number: 4356637,
+		size: "5.0",
+		companyPartner: "SamMobile",
+		manufactured: new Date("2021-11-05"),
 	},
 	{
-		name: "triangle",
-		data: { a: 5, b: 10, c: 14 },
-	},
-	{
-		name: "line",
-		data: { l: 15 },
-	},
-	{
-		name: "circle",
-		data: { r: 10 },
-	},
-	{
-		name: "circle",
-		data: { r: 5 },
-	},
-	{
-		name: "rect",
-		data: { a: 15, b: 7 },
-	},
-	{
-		name: "triangle",
+		company: "Apple",
+		number: 4552833,
+		size: "5.7",
+		companyPartner: "no data",
+		manufactured: new Date("2022-05-24T12:00:00"),
 	},
 ];
 
-console.log(calculateAmountOfFigures(data));
+interface IPhonesManufacturedAfterDate extends IMobilePhone {
+	initialDate: string;
+}
+
+// Функция должна отфильтровать массив данных и вернуть новый массив
+// с телефонами, выпущенными после даты в третьем аргументе
+
+function filterPhonesByDate(
+	phones: IMobilePhone[], 
+	key: keyof IMobilePhone, 
+	initial: string
+): IPhonesManufacturedAfterDate[] {
+	let filteredPhones: IPhonesManufacturedAfterDate[] = [];
+	
+	phones.forEach(obj => {
+		if(obj[key] > new Date(initial)) {
+			filteredPhones.push({
+				...obj,
+				initialDate: initial
+			});
+		}
+	});
+
+	return filteredPhones;
+}
+
+// Второй аргумент при вызове функции должен быть связан с первым,
+// а значит мы получим подсказки - свойства этого объекта
+
+console.log(filterPhonesByDate(phones, "manufactured", "2022-01-01"));
